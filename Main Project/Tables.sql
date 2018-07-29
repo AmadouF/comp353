bb805f76cffedaf1f41f3d5f1250fd21c4eb811a
Swallow666 <liyuan0314@outlook.com>
2018-07-29 13:14:33 -0400

    ud

--- Main Project/Tables.sql
+++ Main Project/Tables.sql
@@ -10,7 +10,7 @@ initalAmount double NOT NULL,
 serviceStartDate DATE NOT NULL,
 serviceType ENUM('Cloud','On-premises') NOT NULL,
 contractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
-lineOfBusiness ENUM('CloudServices', 'Development', 'Research'),
+lineOfBusiness ENUM('CloudServices', 'Development', 'Research') NOT NULL,
 satisfactionLevel int CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10)
 );
 
@@ -21,18 +21,18 @@ Create table Clients(
  repFirstName varchar(255),
  repMiddleInital varchar(5),
  repLastName varchar(255),
- emailId varchar(255), 
- city varchar(255), 
+ emailId varchar(255),
+ city varchar(255),
  province varchar(255),
  postalCode varchar(255),
  password varchar(20),
  PRIMARY KEY (clientId)
  );
- 
+
 CREATE TABLE Employees (
 employeeId INT NOT NULL AUTO_INCREMENT,
 firstName VARCHAR(255) NOT NULL,
-lastName VARCHAR(255) NOT NULL, 
+lastName VARCHAR(255) NOT NULL,
 password varchar(20) NOT NULL,
 PRIMARY KEY (employeeId));
 
@@ -70,16 +70,16 @@ taskType ENUM('Set up infrastructure for client','Provisioning of resources','As
 hours int CHECK(hours >= 0)
 );
 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
  --How to reset primary key
  alter table Companies AUTO_INCREMENT = 1;
- 
+
  --Companies insertion
 INSERT INTO Companies
 VALUES (0,'Apple','Tim',NULL,'Cook','timcook@apple.com','Cupertino','CA','T2P4Y5');
@@ -99,13 +99,3 @@ VALUES (0,1,'5142225646',97000.00, 124.00, '2010-09-19','Cloud','Diamond');
 --Employee insertion
 INSERT INTO Employees
 VALUES(0,1,'Development',1);
-
-
-
-
-
-
-
-
-
-
