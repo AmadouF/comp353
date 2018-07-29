e80bab56ab16abc4edb570a8648c4990aae66268
Swallow666 <liyuan0314@outlook.com>
2018-07-29 15:02:25 -0400

    unicode

--- Main Project/Tables.sql
+++ Main Project/Tables.sql
@@ -1,73 +1,74 @@
 --Main Project, new table additions.
 
 CREATE TABLE Contracts(
-contractId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-clientId int NOT NULL,
-FOREIGN KEY(clientId) REFERENCES Clients(clientId),
-contactNumber varchar(15) NOT NULL,
-annualContractValue double NOT NULL,
-initalAmount double NOT NULL,
-serviceStartDate DATE NOT NULL,
-serviceType ENUM('Cloud','On-premises') NOT NULL,
-contractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
-lineOfBusiness ENUM('CloudServices', 'Development', 'Research') NOT NULL,
-satisfactionLevel int CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10)
+  contractId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  clientId INT NOT NULL,
+  FOREIGN KEY(clientId) REFERENCES Clients(clientId),
+  contactNumber VARCHAR(15) NOT NULL,
+  annualContractValue DOUBLE NOT NULL,
+  initalAmount DOUBLE NOT NULL,
+  serviceStartDate DATE NOT NULL,
+  serviceType ENUM('Cloud','On-premises') NOT NULL,
+  contractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
+  lineOfBusiness ENUM('CloudServices', 'Development', 'Research') NOT NULL,
+  satisfactionLevel INT CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10)
 );
 
 
-Create table Clients(
- clientId int NOT NULL AUTO_INCREMENT,
- clientName varchar(255),
- repFirstName varchar(255),
- repMiddleInital varchar(5),
- repLastName varchar(255),
- emailId varchar(255),
- city varchar(255),
- province varchar(255),
- postalCode varchar(255),
- password varchar(20),
- PRIMARY KEY (clientId)
+CREATE TABLE Clients(
+  clientId INT NOT NULL AUTO_INCREMENT,
+  clientName VARCHAR(255),
+  repFirstName VARCHAR(255),
+  repMiddleInital VARCHAR(5),
+  repLastName VARCHAR(255),
+  emailId VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  postalCode VARCHAR(255),
+  password VARCHAR(20) NOT NULL,
+  PRIMARY KEY (clientId)
  );
 
 CREATE TABLE Employees (
-employeeId INT NOT NULL AUTO_INCREMENT,
-firstName VARCHAR(255) NOT NULL,
-lastName VARCHAR(255) NOT NULL,
-password varchar(20) NOT NULL,
-PRIMARY KEY (employeeId));
+  employeeId INT NOT NULL AUTO_INCREMENT,
+  firstName VARCHAR(255) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  password varchar(20) NOT NULL,
+  PRIMARY KEY (employeeId)
+);
 
 CREATE TABLE SalesAssociate (
-        employeeId int NOT NULL PRIMARY KEY REFERENCES Employees(employeeId)
+  employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId)
 );
 
 CREATE TABLE Admin (
-        employeeId int NOT NULL PRIMARY KEY REFERENCES Employees(employeeId)
+  employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId)
 );
 
 
 CREATE TABLE Manager (
-        employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
-		contractId int NOT NULL,
-		FOREIGN KEY (contractId) REFERENCES Contracts(contractId)
+  employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
+	contractId INT NOT NULL,
+	FOREIGN KEY (contractId) REFERENCES Contracts(contractId)
 );
 
 
 CREATE TABLE Regular (
-        employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
-		contractId int NOT NULL,
-		department ENUM('Development', 'QA', 'UI', 'Design', 'BusinessIntelligence', 'Networking') NOT NULL,
-		insurance ENUM('Premium','Silver','Normal') NOT NULL,
-		FOREIGN KEY (contractId) REFERENCES Contracts(contractId)
+  employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
+	contractId INT NOT NULL,
+	department ENUM('Development', 'QA', 'UI', 'Design', 'BusinessIntelligence', 'Networking') NOT NULL,
+	insurance ENUM('Premium','Silver','Normal') NOT NULL,
+	FOREIGN KEY (contractId) REFERENCES Contracts(contractId)
 );
 
 
 
 CREATE TABLE Tasks(
-employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
-contractId int NOT NULL,
-FOREIGN KEY(contractId) REFERENCES Contracts(contractId),
-taskType ENUM('Set up infrastructure for client','Provisioning of resources','Assigning tasks to resources','Allocating a dedicated point of contact') NOT NULL,
-hours int CHECK(hours >= 0)
+  employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
+  contractId INT NOT NULL,
+  FOREIGN KEY(contractId) REFERENCES Contracts(contractId),
+  taskType ENUM('Set up infrastructure for client','Provisioning of resources','Assigning tasks to resources','Allocating a dedicated point of contact') NOT NULL,
+  hours int CHECK(hours >= 0) default 0
 );
 
 
@@ -78,7 +79,7 @@ hours int CHECK(hours >= 0)
 
 
  --How to reset primary key
- alter table Companies AUTO_INCREMENT = 1;
+ ALTER table Companies AUTO_INCREMENT = 1;
 
  --Companies insertion
 INSERT INTO Companies
