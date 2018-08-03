ed44fd6f5134f7ec787ba1e0a793adbc69ab3240
Swallow666 <liyuan0314@outlook.com>
2018-08-02 22:37:56 -0400

    table update

--- Main Project/Tables.sql
+++ Main Project/Tables.sql
@@ -1,9 +1,9 @@
---Main Project, new table additions.
+--Main Project, table additions.
 
 CREATE TABLE Contracts(
   contractId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   clientId INT NOT NULL,
-  FOREIGN KEY(clientId) REFERENCES Clients(clientId),
+  superviseBy INT NOT NULL,
   contactNumber VARCHAR(15) NOT NULL,
   annualContractValue DOUBLE NOT NULL,
   initalAmount DOUBLE NOT NULL,
@@ -11,12 +11,14 @@ CREATE TABLE Contracts(
   serviceType ENUM('Cloud','On-premises') NOT NULL,
   contractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
   lineOfBusiness ENUM('CloudServices', 'Development', 'Research') NOT NULL,
-  satisfactionLevel INT CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10)
+  satisfactionLevel INT CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10),
+  FOREIGN KEY (clientId) REFERENCES Clients(clientId),
+  FOREIGN KEY (superviseBy) REFERENCES Supervisor(employeeId)
 );
 
 
 CREATE TABLE Clients(
-  clientId INT NOT NULL AUTO_INCREMENT,
+  clientId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   clientName VARCHAR(255),
   repFirstName VARCHAR(255),
   repMiddleInital VARCHAR(5),
@@ -25,16 +27,14 @@ CREATE TABLE Clients(
   city VARCHAR(255),
   province VARCHAR(255),
   postalCode VARCHAR(255),
-  password VARCHAR(20) NOT NULL,
-  PRIMARY KEY (clientId)
+  password VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Employees (
-  employeeId INT NOT NULL AUTO_INCREMENT,
+  employeeId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
   lastName VARCHAR(255) NOT NULL,
-  password varchar(20) NOT NULL,
-  PRIMARY KEY (employeeId)
+  password varchar(20) NOT NULL
 );
 
 CREATE TABLE Supervisor (
@@ -53,9 +53,9 @@ CREATE TABLE Admin (
 CREATE TABLE Manager (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
 	contractId INT NOT NULL,
-  manageBy INT NOT NULL,
+  superviseBy INT NOT NULL,
 	FOREIGN KEY (contractId) REFERENCES Contracts(contractId),
-  FOREIGN KEY (manageBy) REFERENCES Supervisor(employeeId)
+  FOREIGN KEY (superviseBy) REFERENCES Supervisor(employeeId)
 );
 
 
@@ -75,37 +75,7 @@ CREATE TABLE Regular (
 CREATE TABLE Tasks(
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
   contractId INT NOT NULL,
-  FOREIGN KEY(contractId) REFERENCES Contracts(contractId),
   taskType ENUM('Set up infrastructure for client','Provisioning of resources','Assigning tasks to resources','Allocating a dedicated point of contact') NOT NULL,
-  hours int CHECK(hours >= 0) default 0
+  hours int CHECK(hours >= 0) default 0,
+  FOREIGN KEY(contractId) REFERENCES Contracts(contractId)
 );
-
-
-
-
-
-
-
-
---How to reset primary key
-ALTER table Companies AUTO_INCREMENT = 1;
-
---Companies insertion
-INSERT INTO Companies
-VALUES (0,'Apple','Tim',NULL,'Cook','timcook@apple.com','Cupertino','CA','T2P4Y5');
-
-INSERT INTO Companies
-VALUES (0,'Playstation','Kazuo','K','Hirai','khirai@playstation.com','Tokyo','MU','U6QS9W');
-
-UPDATE Companies
-SET proince = 'CA'
-WHERE companyName = 'Apple'
-
-
---Contracts insertion
-INSERT INTO Contracts
-VALUES (0,1,'5142225646',97000.00, 124.00, '2010-09-19','Cloud','Diamond');
-
---Employee insertion
-INSERT INTO Employees
-VALUES(0,1,'Development',1);
