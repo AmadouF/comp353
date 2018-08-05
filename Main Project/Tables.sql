7a7258b96a5aaab7f497f85e1dd29ceb6092e2bd
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2018-08-05 12:22:32 -0400

    Fixed problems in tables and added drop order
    Tasks had a syntax error, so CHECK(...) had to be moved under. Tables are also reordered so that they are in the right order to just copy paste in mysql

--- Main Project/Tables.sql
+++ Main Project/Tables.sql
@@ -1,22 +1,14 @@
---Main Project, table additions.
+-- This is the drop order
+drop table Tasks;
+drop table Regular;
+drop table Manager;
+drop table Contracts;
+drop table SalesAssociate;
+drop table Admins;
+drop table Employees;
+drop table Clients;
 
-CREATE TABLE Contracts(
-  contractId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  clientId INT NOT NULL,
-  superviseBy INT NOT NULL,
-  contactNumber VARCHAR(15) NOT NULL,
-  annualContractValue DOUBLE NOT NULL,
-  initalAmount DOUBLE NOT NULL,
-  serviceStartDate DATE NOT NULL,
-  serviceType ENUM('Cloud','On-premises') NOT NULL,
-  contractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
-  lineOfBusiness ENUM('CloudServices', 'Development', 'Research') NOT NULL,
-  satisfactionLevel INT CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10),
-  FOREIGN KEY (clientId) REFERENCES Clients(clientId),
-  FOREIGN KEY (superviseBy) REFERENCES SalesAssociate(employeeId)
-);
-
-
+-- Main tables
 CREATE TABLE Clients(
   clientId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   clientName VARCHAR(255),
@@ -41,10 +33,25 @@ CREATE TABLE SalesAssociate (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId)
 );
 
-CREATE TABLE Admin (
+CREATE TABLE Admins (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId)
 );
 
+CREATE TABLE Contracts(
+  contractId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  clientId INT NOT NULL,
+  superviseBy INT NOT NULL,
+  contactNumber VARCHAR(15) NOT NULL,
+  annualContractValue DOUBLE NOT NULL,
+  initalAmount DOUBLE NOT NULL,
+  serviceStartDate DATE NOT NULL,
+  serviceType ENUM('Cloud','On-premises') NOT NULL,
+  contractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
+  lineOfBusiness ENUM('CloudServices', 'Development', 'Research') NOT NULL,
+  satisfactionLevel INT CHECK (satisfactionLevel >=1 AND satisfactionLevel <=10),
+  FOREIGN KEY (clientId) REFERENCES Clients(clientId),
+  FOREIGN KEY (superviseBy) REFERENCES SalesAssociate(employeeId)
+);
 
 CREATE TABLE Manager (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
@@ -53,7 +60,6 @@ CREATE TABLE Manager (
 	FOREIGN KEY (contractId) REFERENCES Contracts(contractId),
   FOREIGN KEY (superviseBy) REFERENCES SalesAssociate(employeeId)
 );
-
 
 CREATE TABLE Regular (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
@@ -66,12 +72,11 @@ CREATE TABLE Regular (
   FOREIGN KEY (manageBy) REFERENCES Manager(employeeId)
 );
 
-
-
 CREATE TABLE Tasks(
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
   contractId INT NOT NULL,
   taskType ENUM('Set up infrastructure for client','Provisioning of resources','Assigning tasks to resources','Allocating a dedicated point of contact') NOT NULL,
-  hours int CHECK(hours >= 0) default 0,
-  FOREIGN KEY(contractId) REFERENCES Contracts(contractId)
+  hours int default 0,
+  FOREIGN KEY(contractId) REFERENCES Contracts(contractId),
+  CHECK(hours >= 0)
 );
