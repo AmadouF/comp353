a1c3306acdf3095dac6d1c308748b43fb56ec662
Frederic Desgreniers <fredericdesgreniers@gmail.com>
2018-08-09 13:37:43 -0400

    Reload page when satisfaction changes so information updates

--- Main Project/Website/script.js
+++ Main Project/Website/script.js
@@ -3,6 +3,7 @@ function setSatisfactionLevel(sel, contractId) {
     $.post("client_save_satisfaction_score.php",{"contractId": contractId, "satisfactionScore": sel.value}, function(data) {
         console.log("sucess");
         console.log(data);
+        location.reload();
     }).fail(function() {
         console.log("fail");
     });
