c326d86d4610dd8c8041771f7c08ec571a8fbb27
Swallow666 <liyuan0314@outlook.com>
2018-08-12 15:34:14 -0400

    update table.sql

--- Main Project/Tables.sql
+++ Main Project/Tables.sql
@@ -1,24 +1,24 @@
--- This is the drop order
+drop table Payments;
 drop table Tasks;
 drop table Regular;
 drop table Manager;
+drop table Deliverables;
 drop table Contracts;
 drop table SalesAssociate;
 drop table Admins;
 drop table Employees;
 drop table Clients;
 
--- Main tables
 CREATE TABLE Clients(
   clientId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  clientName VARCHAR(255),
-  repFirstName VARCHAR(255),
-  repMiddleInital VARCHAR(5),
-  repLastName VARCHAR(255),
-  emailId VARCHAR(255),
-  city VARCHAR(255),
-  province VARCHAR(255),
-  postalCode VARCHAR(255),
+  clientName VARCHAR(255) NOT NULL,
+  repFirstName VARCHAR(255) NOT NULL,
+  repMiddleInital VARCHAR(5) NOT NULL,
+  repLastName VARCHAR(255) NOT NULL,
+  emailId VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  postalCode VARCHAR(255) NOT NULL,
   password VARCHAR(20) NOT NULL
 );
 
@@ -53,9 +53,25 @@ CREATE TABLE Contracts(
   FOREIGN KEY (superviseBy) REFERENCES SalesAssociate(employeeId)
 );
 
+create table Payments(
+	paymentId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	contractId INT NOT NULL,
+  FOREIGN KEY (contractId) REFERENCES Contracts(contractId)
+);
+
+create TABLE Deliverables(
+    contractId INT NOT NULL,
+    deliverableIndex INT NOT NULL,
+    scheduledDate DATE NOT NULL,
+    deliveredDate DATE,
+    PRIMARY KEY (contractId, DeliverableIndex),
+    FOREIGN KEY (contractId) REFERENCES Contracts(contractId),
+    CHECK(WEEDDAY(scheduledDate)!=5 AND WEEKDAY(scheduledDate)!=6)
+);
+
 CREATE TABLE Manager (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
-	contractId INT NOT NULL,
+	contractId INT,
   superviseBy INT NOT NULL,
 	FOREIGN KEY (contractId) REFERENCES Contracts(contractId),
   FOREIGN KEY (superviseBy) REFERENCES SalesAssociate(employeeId)
@@ -63,8 +79,8 @@ CREATE TABLE Manager (
 
 CREATE TABLE Regular (
   employeeId INT NOT NULL PRIMARY KEY REFERENCES Employees(employeeId),
-	contractId INT NOT NULL,
-  manageBy INT NOT NULL,
+	contractId INT,
+  manageBy INT,
 	department ENUM('Development', 'QA', 'UI', 'Design', 'BusinessIntelligence', 'Networking') NOT NULL,
 	insurance ENUM('Premium','Silver','Normal') NOT NULL,
   desiredContractType ENUM('Premium','Gold','Diamond','Silver') NOT NULL,
